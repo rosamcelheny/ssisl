3051d9d6adfe8d6c865d279a420aa9892b8dd7e9
rmcelheny <>
2021-06-24 22:02:23 -0400

    get things working

--- script.js
+++ script.js
@@ -16,16 +16,18 @@ var height = $(window).height();
 
 console.log(id);
 
-$("#videos").change(function() {
-  $("h1").hide();
-  id = $(this).val();
-  console.log(id);
-  $("iframe").remove();
-  var player_el = document.createElement("div");
-  $(player_el).attr("id", "player");
-  $("body").append(player_el);
-  makePlayer();
-})
+// $("#videos").change(function() {
+//   $("h1").hide();
+//   $("header").show(); 
+//   $("g").css({"fill": "none", "stroke": "white", "stroke-width": "2px"});    
+//   id = $(this).val();
+//   console.log(id);
+//   $("iframe").remove();
+//   var player_el = document.createElement("div");
+//   $(player_el).attr("id", "player");
+//   $("body").append(player_el);
+//   makePlayer();
+// })
 
 function makePlayer() {
   player = new YT.Player('player', {
@@ -42,38 +44,145 @@ function makePlayer() {
   });
 }
 
-$( "h2" ).click(function() {
-  console.log("Click");
+// TO DO: when you click play, the shapes disappears; when video pauses, they reappear.
+
+// clicking the "i" shows/hides the colophon info
+$(".info").click(function() {
+  $(".caption").toggle(0, function(){
+      // check paragraph once toggle effect is completed
+      if ($(".caption").is(":visible")) {
+          $(".container").css("background-color", "black");
+          $(".info").html("x");
+          $("g").css({"fill": "none", "stroke": "white", "stroke-width": "2px"});
+      } else {
+          $(".container").css("background-color", "white");
+          $(".info").html("i");
+          $("g").css({"fill": "white", "stroke-width": "0px"});
+      }
+  });
+});
+
+// clicking the small title takes you back to landing page
+$("h2").click(function() {
   $("iframe").remove();
+  $(".caption").hide();
   $("h1").show();
-});
-$( ".info" ).click(function() {
-  console.log("Click");
-  $("iframe").remove();
-  $(".caption").toggle();
-});
+  $("header").hide();
+  $("g").css({"fill": "white", "stroke-width": "0px"});
+})
 
-// function onYouTubeIframeAPIReady() {
-//   makePlayer();
-// }
+// for custom select
+var x, i, j, l, ll, selElmnt, a, b, c;
 
-// 4. The API will call this function when the video player is ready.
-// function onPlayerReady(event) {
-//   event.target.playVideo();
-// }
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+l = x.length;
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-// var done = false;
-// function onPlayerStateChange(event) {
-//   if (event.data == YT.PlayerState.PLAYING && !done) {
-//     // setTimeout(stopVideo, 6000);
-//     done = true;
-//   }
-// }
-// function stopVideo() {
-//   player.stopVideo();
-// }
+// for each custom-select element, do the following:
+for (i = 0; i < l; i++) {
 
-// console.log("Hello");
+  // find the first select element
+  selElmnt = x[i].getElementsByTagName("select")[0];
+
+  ll = selElmnt.length;
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /* For each option in the original select element,
+    create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+
+    // this happens when u click any except for the top
+    c.addEventListener("click", function(e) {
+      
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+
+            $("h1").hide();
+            $("header").show(); 
+            $("g").css({"fill": "none", "stroke": "white", "stroke-width": "2px"});
+            var selected_video = $("#videos").val();
+            console.log(selected_video);
+            $("iframe").remove();
+            var player_el = document.createElement("div");
+            $(player_el).attr("id", "player");
+            $("body").append(player_el);
+            // makePlayer();
+            
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+    if ($(".custom-select").css("bottom") == "0px") {
+      height = $(".custom-select").height() * (ll - 1) + "px";
+      console.log(height);
+      // $(".custom-select").animate({bottom: `${height}`});
+      $(".custom-select").animate({bottom: "500px"});
+    } else {
+      $(".custom-select").animate({bottom: "0px"});
+    }
+    
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i);
+    } else {
+      y[i].classList.remove("select-arrow-active");
+      $(".custom-select").animate({bottom: "0px"});
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+      $(".custom-select").animate({bottom: "0px"});
+    }
+  }
+}
+
+/* If the user clicks anywhere outside the select box,
+then close all select boxes: */
+document.addEventListener("click", closeAllSelect);
